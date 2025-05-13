5a6a405f9476dba04365a04e567ba5cef31544e4
gerardocrr <gerardo2551.cr@gmail.com>
2025-05-12 20:26:08 -0600

    refactor Home component layout and update styling

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,25 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        quiz js
+    <div className="flex items-center justify-center bg-red-400 h-dvh">
+      <main className="flex w-full">
+        <div className="w-full flex justify-center">
+          <Link
+            className="bg-white text-black p-2 rounded-md hover:bg-slate-200 flex justify-center w-1/2"
+            href={"/junior"}
+          >
+            Junior
+          </Link>
+        </div>
+        <div className="w-full flex justify-center">
+          <Link
+            className="bg-white text-black p-2 rounded-md hover:bg-slate-200 flex justify-center w-1/2"
+            href={"/midu"}
+          >
+            Midu
+          </Link>
+        </div>
       </main>
     </div>
   );
