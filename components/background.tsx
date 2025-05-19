39959e4119816fbc3efe73d29491a28d45871c69
gerardocrr <gerardo2551.cr@gmail.com>
2025-05-19 00:05:36 -0600

    fix Background component to correctly render the radial gradient background

--- components/background.tsx
+++ components/background.tsx
@@ -9,6 +9,8 @@ export default function Background({ background }: Props) {
   };
 
   return (
-    <div className={`${bg(background)} absolute w-full h-full -z-10`}></div>
+    <div className={`${bg(background)} absolute w-full h-full -z-10`}>
+      <div className="absolute inset-0 -z-10 h-full w-full bg-white [background:radial-gradient(125%_125%_at_50%_10%,#fff_40%,#f7df1e_100%)]"></div>
+    </div>
   );
 }
