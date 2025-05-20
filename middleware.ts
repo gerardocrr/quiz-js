94b9c5377c9e25fd969427bc54f4a84ec76b933c
gerardocrr <gerardo2551.cr@gmail.com>
2025-05-20 10:53:42 -0600

    fix: update route matcher to allow subpaths for protected routes

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,6 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isProtectedRoute = createRouteMatcher(["/midu"]);
+const isProtectedRoute = createRouteMatcher(["/midu(.*)"]);
 
 export default clerkMiddleware(async (auth, req) => {
   if (isProtectedRoute(req)) {
